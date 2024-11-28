9636e2c50ced8df0bccbd3382708515dd73c4520
Melissa Leal <melissalealp@outlook.com>
2024-11-28 02:37:55 -0300

    Estilização

--- components/cardproduto.php
+++ components/cardproduto.php
@@ -1,9 +1,11 @@
-<div class="card text-center d-flex cardpocao justify-content-center">
-  <img src="<?= $img ?>" class="card-img-top  justify-content-center imgpocao">
+<div class="card text-center d-flex cardproduto  border-0 justify-content-center">
+  <div class="divimgproduto">
+    <img src="<?= $img ?>" class="card-img-top  justify-content-center imgproduto img-fluid">
+  </div>
   <div class="card-body">
     <h5 class="card-title"><?= $nome ?></h5>
     <p class="card-text"><?= $tipo ?>.</p>
     <p class="card-text"><?= $preco_unitario ?>.</p>
-    <a href="produto.php?id=<?= $id ?>" class="btn">Saiba mais sobre a poção</a>
+    <a href="produto.php?id=<?= $id ?>" class="btnMais">Saiba mais sobre a poção</a>
   </div>
 </div>
--- components/glasscontainer.php
+++ components/glasscontainer.php
@@ -1,8 +1,5 @@
 <div class="container-fluid glasscontainer m-4">
     <div class="row g-3">
-        <h2> <?= $titulo; ?> </h2>
-    </div>
-    <div class="row g-3">
-        <a href="<?= $caminho; ?>"><button class="btn"><?= $botao; ?></button></a>
+        <h2> <?= $msg; ?> </h2>
     </div>
 </div>
